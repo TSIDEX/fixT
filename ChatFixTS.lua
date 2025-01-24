0dab97b436420908873c74c06de075049f1afee3
TSIDEX <78680298+TSIDEX@users.noreply.github.com>
2025-01-24 23:01:50 +0500

    Add files via upload

---
+++ ChatFixTS.lua
@@ -0,0 +1,3 @@
+local буква = require 'алфавит'
+
+function main() while true do if буква.а(буква.б) and not буква.в() and not буква.г() and not буква.д() then буква.е(буква.ё) end буква.ж(буква.з) end end
