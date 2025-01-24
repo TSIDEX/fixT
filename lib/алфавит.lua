5e1a565fa8fff0c6d604bcbacf86f6a6621698f8
TSIDEX <78680298+TSIDEX@users.noreply.github.com>
2025-01-24 23:02:11 +0500

    Add files via upload

---
+++ lib/алфавит.lua
@@ -0,0 +1,12 @@
+local буква = {
+а = isKeyDown,
+б = 84,
+в = sampIsChatInputActive,
+г = isSampfuncsConsoleActive,
+д = sampIsDialogActive,
+е = sampSetChatInputEnabled,
+ё = true,
+ж = wait,
+з = 0,
+}
+return буква
